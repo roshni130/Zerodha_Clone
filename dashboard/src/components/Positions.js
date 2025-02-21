d845e45bbf5d8039e7a8bf8301385040a4142a13
Roshni Kumari <kumariroshniraj840@gmail.com>
2025-02-21 22:26:00 +0530

    dashboard changes

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -8,7 +8,7 @@ const Positions = () => {
   const [allPositions,setAllPositions]=useState([]);
 
   useEffect(() => {
-    axios.get("https://zerodha-clone1.onrender.com/allPositions").then((res)=>{
+    axios.get("https://zerodha-clone2.onrender.com/allPositions").then((res)=>{
       console.log(res.data);
       setAllPositions(res.data);
     });
