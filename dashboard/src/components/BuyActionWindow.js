0e8e9e0ee36af40fb79a4f9c01893003208433ed
Roshni Kumari <kumariroshniraj840@gmail.com>
2025-02-22 20:11:24 +0530

    dashboard change

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -11,7 +11,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick=() => {
-    axios.post("http://localhost:3002/newOrder",{
+    axios.post("https://zerodha-clone2.onrender.com/newOrder",{
       name:uid,
       qty:stockQuantity,
       price:stockPrice,
